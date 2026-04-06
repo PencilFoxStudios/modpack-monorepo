a475ec2d86afbc6da634e811b37ed9f6e995d835
Liam <lhw2837@rit.edu>
2026-04-06 16:56:00 -0400

    [cr] [sr] Sound Physics + moss eating

---
+++ modpacks/galaxypackq22026/kubejs/server_scripts/eat_moss.js
@@ -0,0 +1,24 @@
+// kubejs/server_scripts/eat_moss.js
+
+function mossEffect(item) {
+  item.foodProperties = (food) => {
+    food.hunger(2);
+    food.saturation(0.2);
+    food.alwaysEdible();
+    // poison for 0.5 seconds with an amplifier of 1 and a 50% chance to apply
+    food.effect("minecraft:poison", 0.5, 1, 0.5);
+    // put in chat that the player has eaten moss and is now poisoned
+    food.onEat((player) => {
+      player.sendMessage("YOU MOSS-EATER!");
+    });
+  };
+}
+
+ItemEvents.modification((event) => {
+  event.modify("minecraft:moss_block", (item) => {
+    mossEffect(item);
+  });
+  event.modify("minecraft:spore_blossom", (item) => {
+    mossEffect(item);
+  });
+});
