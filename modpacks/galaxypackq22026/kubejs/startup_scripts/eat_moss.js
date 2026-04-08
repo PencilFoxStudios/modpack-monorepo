357ff7e0325fc0c843f2e7a6553a73bec554f7d6
Liam <lhw2837@rit.edu>
2026-04-08 14:27:34 -0400

    [cr] [sr] Whoops previous fix ahd a typo.

--- modpacks/galaxypackq22026/kubejs/startup_scripts/eat_moss.js
+++ modpacks/galaxypackq22026/kubejs/startup_scripts/eat_moss.js
@@ -1,13 +1,13 @@
 // kubejs/server_scripts/eat_moss.js
 
 function mossEffect(item) {
-  item.setFoodProperties = (food) => {
+  item.setFoodProperties((food) => {
     food.hunger(2);
     food.saturation(0.2);
     food.alwaysEdible();
     // poison for 0.5 seconds with an amplifier of 1 and a 50% chance to apply
     food.effect("minecraft:poison", 0.5, 1, 0.5);
-  };
+  });
 }
 
 ItemEvents.modification((event) => {
