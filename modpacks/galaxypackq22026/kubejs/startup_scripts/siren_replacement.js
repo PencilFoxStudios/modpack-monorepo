d4a8d7656bee9f722fa30a83e23b73e3f7a8fba0
Liam <lhw2837@rit.edu>
2026-04-09 18:19:28 -0400

    [cr] [sr] Added back weather mod and changed config. Also changed siren to something a little less threatening and possibly a little more familiar. Also disabled Big moon in favor a few other... balance stuff.

---
+++ modpacks/galaxypackq22026/kubejs/startup_scripts/siren_replacement.js
@@ -0,0 +1,50 @@
+// ForgeEvents.onEvent(
+//   "net.minecraftforge.client.event.sound.SoundEvent",
+//   (event) => {
+//     global.SoundEvent(event);
+//   },
+// );
+// StartupEvents.registry("sound_event", (event) => {
+//   event.create("siren");
+// });
+// let sirenActive = false;
+// global.SoundEvent = (event) => {
+//   if (Client.player != null) {
+//     let sound = event.getSound();
+//     if (sound) {
+//       let location = sound.location.toString();
+//       if (location == "weather2:streaming.siren") {
+//         // get location of sound
+//         let x = sound.getX();
+//         let y = sound.getY();
+//         let z = sound.getZ();
+//         console.log("Siren sound detected at " + x + ", " + y + ", " + z);
+//         if (!sirenActive) {
+//           sirenActive = true;
+//           Client.player.tell(
+//             "Siren is not active, playing custom sound and setting sirenActive to true.",
+//           );
+
+//           Client.level.playLocalSound(
+//             x,
+//             y,
+//             z,
+//             "kubejs:siren",
+//             "neutral",
+//             2.0,
+//             1.0,
+//             true
+//           );
+//           // after 96 seconds, set sirenActive to false
+//           Client.schedule(96000, () => {
+//             sirenActive = false;
+//           });
+//         } else {
+//           Client.player.tell(
+//             "Siren is already active, not playing custom sound.",
+//           );
+//         }
+//       }
+//     }
+//   }
+// };
